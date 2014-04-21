99a31439435f09acc71fde70d295b0d7d6218d9d
Samuel <samuel@seuranta.org>
2014-04-21 21:19:57 +0300

    remove creds file from version control, offer a sample file instead

---
+++ creds-sample.php
@@ -0,0 +1,10 @@
+<?php
+
+// MySQL Credentials
+$db_host = "localhost";
+$db_user = "";     // Enter your MySQL username
+$db_pass = "";     // Enter your MySQL password
+$db_name = "torque";
+$db_table = "raw_logs";
+
+?>
