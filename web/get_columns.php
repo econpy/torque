aad5c78a3a752e9f36ec790d9fe1b826a3fb9948
Matt Nicklay <matthew.nicklay@gmail.com>
2014-08-25 17:12:07 -0500

    Fixing MySQL syntax for consistency

--- web/get_columns.php
+++ web/get_columns.php
@@ -1,14 +1,15 @@
 <?php
-require("./creds.php");
+require 'creds.php';
 
 // Connect to Database
-mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
-mysql_select_db("INFORMATION_SCHEMA") or die(mysql_error());
+$con = mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
+mysql_select_db("INFORMATION_SCHEMA", $con) or die(mysql_error());
 
 // Create array of column name/comments for chart data selector form
 $colqry = mysql_query("SELECT COLUMN_NAME,COLUMN_COMMENT,DATA_TYPE
-                           FROM COLUMNS WHERE TABLE_SCHEMA='".$db_name."'
-                           AND TABLE_NAME='".$db_table."'") or die(mysql_error());
+                       FROM COLUMNS
+                       WHERE TABLE_SCHEMA='".$db_name."'
+                       AND TABLE_NAME='".$db_table."'", $con) or die(mysql_error());
 
 // Select the column name and comment for data that can be plotted.
 while ($x = mysql_fetch_array($colqry)) {
@@ -18,5 +19,8 @@ while ($x = mysql_fetch_array($colqry)) {
 }
 
 $numcols = strval(count($coldata)+1);
+
+mysql_free_result($colqry);
+mysql_close($con);
 
 ?>
