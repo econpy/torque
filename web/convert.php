b0467e8b6c3518294e11c2b02914c284e0145240
Matt Nicklay <matthew.nicklay@gmail.com>
2014-08-25 16:25:45 -0500

    Fixing MySQL syntax for consistency

--- web/convert.php
+++ web/convert.php
@@ -2,7 +2,7 @@
 require 'creds.php';
 
 $con = mysql_connect($db_host, $db_user, $db_pass) or die(mysql_error());
-mysql_select_db($db_name) or die(mysql_error());
+mysql_select_db($db_name, $con) or die(mysql_error());
 
 // User Units
 $userunitqry = mysql_query("SELECT column_name AS userkey
