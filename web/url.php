2565d5ccf43a538b2123c74c9fae09dfd2d6a4e8
Matt Nicklay <matthew.nicklay@gmail.com>
2014-08-25 17:28:04 -0500

    Added HTTP/HTTPS check

--- web/url.php
+++ web/url.php
@@ -1,38 +1,47 @@
 <?php
 session_start();
 
+$isSecure = false;
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') {
+    $isSecure = true;
+}
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https' || !empty($_SERVER['HTTP_X_FORWARDED_SSL']) && $_SERVER['HTTP_X_FORWARDED_SSL'] == 'on') {
+    $isSecure = true;
+}
+$REQUEST_PROTOCOL = $isSecure ? 'https' : 'http';
+
 // Get the Full URL to the session.php file
-$thisfile = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+$thisfile = "$REQUEST_PROTOCOL://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $parts = strtok("url.php", $thisfile);
 if (isset($_GET["makechart"])) {
-    $baselink = $parts["0"]."session.php";
+    $baselink = $parts['0']."session.php";
     if (isset($_GET["seshid"])) {
         $seshid = strval(mysql_escape_string($_GET["seshid"]));
         if (isset($_POST["plotdata"])) {
             $plotdataarray = $_POST["plotdata"];
             $s1data = $plotdataarray[0];
             $s2data = $plotdataarray[1];
-            $outurl = $baselink."?id=".$seshid."&s1=".$s1data."&s2=".$s2data;
+            $outurl = "$baselink?id=$seshid&s1=$s1data&s2=$s2data";
         }
         else {
             $seshid = $_SESSION['recent_session_id'];
-            $outurl = $baselink."?id=".$seshid;
+            $outurl = "$baselink?id=$seshid";
         }
     }
     else {
         $seshid = $_SESSION['recent_session_id'];
-        $outurl = $baselink."?id=".$seshid;
+        $outurl = "$baselink?id=$seshid";
     }
 }
 else {
-    $baselink = $parts["0"]."session.php";
+    $baselink = $parts['0']."session.php";
     if (isset($_POST["seshidtag"])) {
         $seshid = strval(mysql_escape_string($_POST["seshidtag"]));
-        $outurl = $baselink."?id=".$seshid;
+        $outurl = "$baselink?id=$seshid";
     }
     else {
         $seshid = $_SESSION['recent_session_id'];
-        $outurl = $baselink."?id=".$seshid;
+        $outurl = "$baselink?id=$seshid";
     }
 }
 
