a141ecaf62e0c6eba9e7de0152672296ba714241
surfrock66 <surfrock66@surfrock66.com>
2016-03-23 13:21:01 -0700

    So, I am an idiot. All my testing was uploading from the app to the wrong instance on my server, so I never really tested upload_data.php.  I fixed that, tested it, found a jillion bugs, FIXED THOSE, and now we should be in better shape.  My bad.

--- web/upload_data.php
+++ web/upload_data.php
@@ -9,84 +9,87 @@ mysql_select_db($db_name, $con) or die(mysql_error());
 // Create an array of all the existing fields in the database
 $result = mysql_query("SHOW COLUMNS FROM $db_table", $con) or die(mysql_error());
 if (mysql_num_rows($result) > 0) {
-    while ($row = mysql_fetch_assoc($result)) {
-        $dbfields[]=($row['Field']);
-    }
+  while ($row = mysql_fetch_assoc($result)) {
+    $dbfields[]=($row['Field']);
+  }
 }
 
 // Iterate over all the k* _GET arguments to check that a field exists
 if (sizeof($_GET) > 0) {
-    $keys = array();
-    $values = array();
-    $sesskeys = array();
-    $sessvalues = array();
-    $sessuploadid = array();
-    foreach ($_GET as $key => $value) {
-        // Keep columns starting with k
-        if (preg_match("/^k/", $key)) {
-            $keys[] = $key;
-            $values[] = $value;
-            $submitval = 1;
-        }
-        else if (in_array($key, array("v", "eml", "time", "id", "session", "profileName", "profile", "notice", "noticeClass"))) {
-            if ($key == 'id') {
-              $sessuploadid = $value;
-            }
-            $keys[] = $key;
-            $values[] = "'".$value."'";
-            $sesskeys[] = $key;
-            $sessvalues[] = "'".$value."'";
-            $submitval = 1;
-        }
-        // Skip columns matching userUnit*, defaultUnit*, and profile
-        else if (preg_match("/^userUnit/", $key) or preg_match("/^defaultUnit/", $key))))) {
-        //else if (preg_match("/^userUnit/", $key) or preg_match("/^defaultUnit/", $key) or (preg_match("/^profile/", $key)))) {
-            $submitval = 0;
-        }
-        else {
-            $submitval = 0;
-        }
-        // NOTE: Use the following "else" statement instead of the one above
-        //       if you want to keep anything else.
-        //else {
-        //    $keys[] = $key;
-        //    $values[] = "'".$value."'";
-        //    $submitval = 1;
-        //}
-        // If the field doesn't already exist, add it to the database
-        if (!in_array($key, $dbfields) and $submitval == 1) {
-            if ( is_float($value) ) {
-                $sqlalter = "ALTER TABLE $db_table ADD $key float NOT NULL default '0'";
-                $sqlalterkey = "INSERT INTO $db_keys_table (id, description, type, populated) VALUES ('$key','$key','float',1)";
-            } else {
-                $sqlalter = "ALTER TABLE $db_table ADD $key VARCHAR(255) NOT NULL default '0'";
-                $sqlalterkey = "INSERT INTO $db_keys_table (id, description, type, populated) VALUES ('$key','$key','varchar(255)',1)";
-            }
-            mysql_query($sqlalter, $con) or die(mysql_error());
-            mysql_query($sqlalterkey, $con) or die(mysql_error());
-        }
+  $keys = array();
+  $values = array();
+  $sesskeys = array();
+  $sessvalues = array();
+  $sessuploadid = "";
+  $sesstime = "0";
+  foreach ($_GET as $key => $value) {
+    if (preg_match("/^k/", $key)) {
+      // Keep columns starting with k
+      $keys[] = $key;
+      $values[] = $value;
+      $submitval = 1;
+    } else if (in_array($key, array("v", "eml", "time", "id", "session", "profileName", "profile", "notice", "noticeClass"))) {
+      // Keep non k* columns listed here
+      if ($key == 'session') {
+        $sessuploadid = $value;
+      }
+      if ($key == 'time') {
+        $sesstime = $value;
+      }
+      $keys[] = $key;
+      $values[] = "'".$value."'";
+      $sesskeys[] = $key;
+      $sessvalues[] = "'".$value."'";
+      $submitval = 1;
+    } else {
+      $submitval = 0;
     }
-    if ((sizeof($keys) === sizeof($values)) && sizeof($keys) > 0) {
-        // Now insert the data for all the fields
-        $sql = "INSERT INTO $db_table (".implode(",", $keys).") VALUES (".implode(",", $values).")";
-        mysql_query($sql, $con) or die(mysql_error());
+    // If the field doesn't already exist, add it to the database
+    if (!in_array($key, $dbfields) and $submitval == 1) {
+      if ( is_float($value) ) {
+        // Add field if it's a float
+        $sqlalter = "ALTER TABLE $db_table ADD $key float NOT NULL default '0'";
+        $sqlalterkey = "INSERT INTO $db_keys_table (id, description, type, populated) VALUES ('$key','$key','float',1)";
+      } else {
+        // Add field if it's a string, specifically varchar(255)
+        $sqlalter = "ALTER TABLE $db_table ADD $key VARCHAR(255) NOT NULL default '0'";
+        $sqlalterkey = "INSERT INTO $db_keys_table (id, description, type, populated) VALUES ('$key','$key','varchar(255)',1)";
+      }
+      mysql_query($sqlalter, $con) or die(mysql_error());
+      mysql_query($sqlalterkey, $con) or die(mysql_error());
     }
-    if ((sizeof($sesskeys) === sizeof($sessvalues)) && sizeof($sesskeys) > 0) {
-        // Now insert the data for all the fields
-        $sql = "INSERT INTO $db_session_table (".implode(",", $sesskeys).") VALUES (".implode(",", $sessvalues).")";
-        mysql_query($sql, $con) or die(mysql_error());
-        $sessionqrystring = "SELECT MIN(time) as `MinTime`, MAX(time) as `MaxTime`, count(*) as 'sessionsize'  FROM $db_table WHERE id LIKE '$sessuploadid'";
-        $sessionqry = mysql_query($sessionqrystring, $con) or die(mysql_error());
-        while($row = mysql_fetch_assoc($sessionqry)) {
-          $sessionqrystring = "UPDATE $db_session_table SET timestart='$row[\"MinTime\"]', timestop='$row[\"MaxTime\"]', sessionsize='$row[\"sessionsize\"]' WHERE id LIKE '$sessuploadid'";
-          mysql_query($sessionqrystring, $con) or die(mysql_error());
-        }
+  }
+  if ((sizeof($keys) === sizeof($values)) && sizeof($keys) > 0) {
+    // Now insert the data for all the fields into the raw logs table
+    $sql = "INSERT INTO $db_table (".implode(",", $keys).") VALUES (".implode(",", $values).")";
+    mysql_query($sql, $con) or die(mysql_error());
+  }
+  // The way session uploads work, there's a separate HTTP call for each datapoint.  This is why raw logs is
+  //  so huge, and has so much repeating data. This is my attempt to flatten the redundant data into the
+  //  sessions table; this code checks if there is already a row for the current session, and if there is, only 
+  //  update the ending time and the count of datapoints.  If there isn't a row, insert one.
+  if ((sizeof($sesskeys) === sizeof($sessvalues)) && sizeof($sesskeys) > 0) {
+    // See if there is already an entry in the sessions table for this session
+    $sessionqrystring = "SELECT session, sessionsize FROM $db_sessions_table WHERE session LIKE '$sessuploadid'";
+    $sessionqry = mysql_query($sessionqrystring, $con) or die(mysql_error());
+    if (mysql_num_rows($sessionqry) > 0) {
+      // If there's an entry in the session table for this session, update the session end time and the datapoint count
+      while($row = mysql_fetch_assoc($sessionqry)) {
+        $sesssizecount = $row["sessionsize"] + 1;
+        $sessionqrystring = "UPDATE $db_sessions_table SET timeend='$sesstime', sessionsize='$sesssizecount' WHERE session LIKE '$sessuploadid'";
+        mysql_query($sessionqrystring, $con) or die(mysql_error());
+      }
+    } else {
+      // If this is a new session, insert an entry in the sessions table and then update the start time and datapoint count
+      $sql = "INSERT INTO $db_sessions_table (".implode(",", $sesskeys).") VALUES (".implode(",", $sessvalues).")";
+      mysql_query($sql, $con) or die(mysql_error());
+      $sql = "UPDATE $db_sessions_table SET timestart='$sesstime', sessionsize='1' WHERE session LIKE '$sessuploadid'";
+      mysql_query($sql, $con) or die(mysql_error());
     }
+  }
 }
-
 mysql_close($con);
 
 // Return the response required by Torque
 echo "OK!";
-
 ?>
